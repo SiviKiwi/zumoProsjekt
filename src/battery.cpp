478ac0dd988ec1b0e4191c1342401e8eb85dacf8
alvargu <a.guddingsmo@outlook.com>
2022-03-15 15:02:39 +0100

    filter small speed values

--- src/battery.cpp
+++ src/battery.cpp
@@ -12,36 +12,42 @@ float speed;
 float prevDist = 0;
 float currentCapacity = 1200.0 * 3600;
 
-float getDistance(int16_t encoderCount) {
-  float rotasjoner = (float) encoderCount / (75.81 * 12.0);
+float getDistance(int16_t encoderCount)
+{
+  float rotasjoner = (float)encoderCount / (75.81 * 12.0);
   float cm = rotasjoner * 12.2522;
   return cm;
 }
 
-float getSpeed(float distance, unsigned long ms) {
-  float cmSecond = distance / ((float) ms / 1000);
+float getSpeed(float distance, unsigned long ms)
+{
+  float cmSecond = distance / ((float)ms / 1000);
   return cmSecond;
 }
 
-
-
-float setCapacity(float speed, unsigned long ms) {
+float setCapacity(float speed, unsigned long ms)
+{
+  if (abs(speed) < 0.1)
+  {
+    speed = 0;
+  }
 
   float currentUsage = 2.0 * abs(speed) + 10.0;
 
-  currentCapacity -= currentUsage * (float) ms / 1000.0;
+  currentCapacity -= currentUsage * (float)ms / 1000.0;
 
   return currentCapacity;
-
 }
 
-void setup() {
+void setup()
+{
   display.init();
 
   time = millis();
 }
 
-void loop() {
+void loop()
+{
 
   int16_t countLeft = encoders.getCountsLeft();
   int16_t countRight = encoders.getCountsRight();
@@ -52,18 +58,18 @@ void loop() {
 
   unsigned long timeNow = millis();
 
-  if (timeNow - time > 100) {
+  if (timeNow - time > 100)
+  {
     unsigned long elapsedTime = timeNow - time;
     speed = getSpeed(dist - prevDist, elapsedTime);
 
     setCapacity(speed, elapsedTime);
 
     display.clear();
-    display.gotoXY(0,0);
-    display.print((float) currentCapacity / 3600.0);
+    display.gotoXY(0, 0);
+    display.print((float)currentCapacity / 3600.0);
 
     time = millis();
     prevDist = dist;
-
   }
 }
