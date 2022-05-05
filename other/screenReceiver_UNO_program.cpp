62921231c20f213da40b9e1fe12779d638c4e437
SiviKiwi <lauvdalsivert@gmail.com>
2022-05-05 15:24:43 +0200

    Lagt til kommentarer

--- other/screenReceiver_UNO_program.cpp
+++ other/screenReceiver_UNO_program.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+// Bibliotek for i2c kommunikasjon
 #include <Wire.h>
+
+// Bibliotek for display
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -9,9 +12,11 @@
 
 String c = "c";
 
+// Setter opp display med bredde og høyde
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 
+// Viser teks på disply
 void displayText(String text){
   
   display.clearDisplay();
@@ -22,20 +27,24 @@ void displayText(String text){
   }
 
 void setup() {
-  // put your setup code here, to run once:
 
+  // Starter seriellkommunikasjon med ESP
   Serial.begin(115200);
   
 
+  // Sjekker om Arduino klarer å koble til display
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;);
   }
 
+  // Venter 2 sek
   delay(2000);
+  // setter opp display med tekstfarge og størrelse
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(2);
+  // Printer en testmelding under oppstart
   display.println("Iver er kul");
   display.display(); 
 
@@ -45,11 +54,14 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 
+  // Sjekker om det er kommet data inn i Arduinoens buffer
 if (Serial.available() > 0)
 {
+  //henter ut strengen fra seriellbufferen
     c = Serial.readString();
 }
 
+// Viser teksten på skjermen
 displayText(c);
 Serial.println(c);
 //c = "c";
